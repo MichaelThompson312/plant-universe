c66b2f8ce85d4b15ff72cc75ab3a28e548944df4
Michael Thompson <Michael@Cool-Imac.local>
2020-03-16 22:54:38 -0500

    Adding Authentication for the User on the backend with JSON web tokens

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,3 +1,6 @@
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+
 require('dotenv').config({ path: 'variables.env' });
 const createServer = require('./createServer');
 const db = require('./db');
@@ -6,7 +9,17 @@ const server = createServer();
 
 //TODO use express middleware to handle cokies (JWT)
 
-//TODO use express middleware to populate current user
+server.express.use(cookieParser());
+
+//Decode the jwt
+server.express.use((req, res, next) => {
+	const { token } = req.cookies;
+	if (token) {
+		const { userId } = jwt.verify(token, process.env.APP_SECRET);
+		req.userId = userId;
+	}
+	next();
+});
 
 server.start(
 	{
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,8 +1,22 @@
 const { forwardTo } = require('prisma-binding');
 
 const Query = {
-	items: forwardTo('db')
-	//async items(paretns, args, ctx, info) {
+	items: forwardTo('db'),
+	item: forwardTo('db'),
+	itemsConnection: forwardTo('db'),
+	me(parent, args, ctx, info) {
+		if (!ctx.request.userId) {
+			return null;
+		}
+		return ctx.db.query.user(
+			{
+				where: { id: ctx.request.userId }
+			},
+			info
+		);
+	}
+
+	//async items(paretns,args, ctx, info) {
 	//	const items = await ctx.db.query.items();
 	//	return items;
 	//}
